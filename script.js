e34ad67b084c40f801e1735038faee94f0f95eb2
Urrabbit1 <131398689+Urrabbit1@users.noreply.github.com>
2025-11-21 23:20:27 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.input-container input').forEach(input => {
+            input.addEventListener('input', function() {
+                // Метка автоматически скрывается через CSS когда поле не пустое
+            });
+        });
